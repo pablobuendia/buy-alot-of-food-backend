302ce9964c7c62208196a669a5f17efb18534ad7
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2022-04-22 16:21:15 -0300

    create first module

--- app.js
+++ app.js
@@ -1,29 +1,4 @@
-console.log("Hello world")
+const {juan, pedro, sayHi} = require('./utils')
 
-let a = 1;
-let b = 9;
-
-console.log(`The result of a + b is ${a + b}`);
-
-
-if (a + b === 10) {
-    console.log("The sum is 10! Congrats! (?)")
-} else {
-    console.log("The sum is less than 10! Great! (?)");
-}
-
-// Just checking some global variables
-console.log(__dirname);
-console.log(__filename);
-console.log(module);
-
-const juan = 'juan'
-const pedro = 'pedro'
-
-const sayHi = (name) => {
-    console.log(`Hello there ${name}`);
-}
-
-sayHi('Susana')
 sayHi(juan)
 sayHi(pedro)
