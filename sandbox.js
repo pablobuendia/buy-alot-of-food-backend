b385de402f9ba84ba77c0778f11609238a8e942b
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2022-04-22 16:21:07 -0300

    move test code to sandbox

---
+++ sandbox.js
@@ -0,0 +1,18 @@
+console.log("Hello world")
+
+let a = 1;
+let b = 9;
+
+console.log(`The result of a + b is ${a + b}`);
+
+
+if (a + b === 10) {
+    console.log("The sum is 10! Congrats! (?)")
+} else {
+    console.log("The sum is less than 10! Great! (?)");
+}
+
+// Just checking some global variables
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(module);
